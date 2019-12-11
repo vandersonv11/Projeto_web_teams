5c1ec04402f64c6ea2b8134bfeccc1fd574816dc
Vanderson Oliveira <vandersonv11@hotmail.com>
2019-12-11 19:52:56 -0300

    Atualiação do documento do projeto, adiconando equipes

--- js/controller/listaTeamsCtrl.js
+++ js/controller/listaTeamsCtrl.js
@@ -1,21 +1,25 @@
+var app = angular.module('listaTeams', []);
 app.controller("listaTeamsCtrl", function($scope, $http) {
   $scope.teams = [];
   //GET
   var carregarTeams = function() {
     $http.get('http://localhost:3000').then(function(res) {
+      console.log(res);
       $scope.teams = res.data;
     });
   };
 
+  // POST
   $scope.adicionarTeam = function(team) {
     team.data = new Date();
-    $http.post("http://localhost:3000", team).success(function(data) {
+    $http.post("http://localhost:3000", team).then(function(data) {
       delete $scope.team;
       $scope.teamForm.$setPristine();
-      carregarTeam();
+      carregarTeams();
     });
   };
 
+  // Del
   $scope.apagarTeam = function(teams) {
     $scope.teams = teams.filter(function(team) {
       if (!team.selecionado) return team;
@@ -28,9 +32,11 @@ app.controller("listaTeamsCtrl", function($scope, $http) {
     });
   };
 
-  $scope.ordenarPor = function (campo) {
-				$scope.criterioDeOrdenacao = campo;
-				$scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
-			};
+  // ordenação
+  $scope.ordenarPor = function(campo) {
+    $scope.criterioDeOrdenacao = campo;
+    $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+  };
+
   carregarTeams();
 });
